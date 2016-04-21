0e60d59b1f3f86fafaa8ede72666e6f8bb446534
LizZ <villeda.e@gmail.com>
2016-04-20 22:37:07 -0600

    Ejercicio 1 - Lex sencillo

---
+++ 1-expr/tokens.h
@@ -0,0 +1,15 @@
+#ifndef _TOKENS_H_
+#define _TOKENS_H_
+
+#define TK_ADD      1
+#define TK_MULT     2
+#define TK_OPENP    3
+#define TK_CLOSEP   4
+#define TK_EQUAL    5
+#define TK_ID       6
+#define TK_NUM      7
+#define TK_SEMIC    8
+#define TK_ASSIGN   9
+#define TK_EOF      10 
+
+#endif
