e41e5a2c467b1ff816d4110dff1171b3009b5d80
LizZ <villeda.e@gmail.com>
2016-04-20 23:52:22 -0600

    Ejercicio 1 - cout

--- 1-expr/tokens.h
+++ 1-expr/tokens.h
@@ -1,15 +1,15 @@
 #ifndef _TOKENS_H_
 #define _TOKENS_H_
 
-#define TK_ADD      1
-#define TK_MULT     2
-#define TK_OPENP    3
-#define TK_CLOSEP   4
-#define TK_EQUAL    5
-#define TK_ID       6
-#define TK_NUM      7
-#define TK_SEMIC    8
-#define TK_ASSIGN   9
-#define TK_EOF      10 
+#define TK_ADD      10
+#define TK_MULT     11
+#define TK_OPENP    12
+#define TK_CLOSEP   13
+#define TK_EQUAL    14
+#define TK_ID       15
+#define TK_NUM      16
+#define TK_SEMIC    17
+#define TK_ASSIGN   18
+#define TK_EOF      19 
 
 #endif
