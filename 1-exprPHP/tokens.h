55b76ea15f34c16a30a441a2b7e6c9bb1aee7973
LizZ <villeda.e@gmail.com>
2016-04-21 19:11:41 -0600

    Ejercicio 2 - PHP
    Variacion de la tarea anterior.
    Uso de extados en FLEX.

---
+++ 1-exprPHP/tokens.h
@@ -0,0 +1,19 @@
+#ifndef _TOKENS_H_
+#define _TOKENS_H_
+
+#define TK_ADD      10
+#define TK_MULT     11
+#define TK_OPENP    12
+#define TK_CLOSEP   13
+#define TK_EQUAL    14
+#define TK_ID       15
+#define TK_INT      16
+#define TK_SEMIC    17
+#define TK_ASSIGN   18
+#define TK_EOF      19
+
+#define TK_FLOAT    20
+#define TK_ERROR    21
+#define TK_COMMENT  22
+
+#endif
